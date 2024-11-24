f626d5ada8572221db7b3011fff22f54971a51b7
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2024-11-24 12:36:11 +0500

    .

--- apps/landing_page/urls.py
+++ apps/landing_page/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import LandingView
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-
+    path( "", (LandingView.as_view(template_name="landing_layout/main.html")), name="index",),
+    path( "contact-me", (LandingView.as_view(template_name="landing_pages/contact_me.html")), name="contact_me",),
+    path( "partner-schools", (LandingView.as_view(template_name="landing_pages/partner_schools.html")), name="partner_schools",),
 ]
--- apps/landing_page/views.py
+++ apps/landing_page/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from account.models import Roles, Regions, District, Quarters
+from web_project import TemplateLayout
+
+
+class LandingView(TemplateView):
+    # template_name = "add_administrator.html"
+
+    def get_context_data(self, **kwargs):
+        # Asosiy layoutni qo'shish
+        context = TemplateLayout.init(self, super().get_context_data(**kwargs))
+
+        # Barcha rollar, viloyatlar, tumanlar va mahallalarni yuborish
+        context['roles'] = Roles.objects.all()
+        context['regions'] = Regions.objects.all()
+        context['districts'] = District.objects.all()
+        context['quarters'] = Quarters.objects.all()
+
+        # Hamkor maktablar ro'yxati
+        context['schools'] = [
+            {
+                "id": 1,
+                "name": "Toshkent Maktabi",
+                "description": "Toshkent maktabi yuqori sifatli ta'lim berish, xalqaro yondashuvni taklif etishi bilan tanilgan.",
+                "image_url": "/static/images/school1.jpg",
+            },
+            {
+                "id": 2,
+                "name": "Namangan Litseyi",
+                "description": "Namangan litseyi har bir talabaga individual yondashuvni taqdim etadi va yuqori natijalarga erishmoqda.",
+                "image_url": "/static/images/school2.jpg",
+            },
+            {
+                "id": 3,
+                "name": "Andijon Akademiyasi",
+                "description": "Andijon Akademiyasi xalqaro standartlarga mos keladigan ta'lim dasturini amalga oshiradi.",
+                "image_url": "/static/images/school3.jpg",
+            },
+            {
+                "id": 4,
+                "name": "Buxoro Kolleji",
+                "description": "Buxoro kollejida talabalar innovatsion texnologiyalardan foydalangan holda ta'lim olishadi.",
+                "image_url": "/static/images/school4.jpg",
+            },
+            {
+                "id": 5,
+                "name": "Samarqand Litseyi",
+                "description": "Samarqand litseyi ilmiy yutuqlarga yo'naltirilgan yuqori sifatli ta'lim dasturini taklif etadi.",
+                "image_url": "/static/images/school5.jpg",
+            },
+        ]
+
+        return context
