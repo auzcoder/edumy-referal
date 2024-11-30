ad226d59fc65bbe4f1fb240591c7ff9ae13e12f1
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2024-11-30 17:06:23 +0500

    ss

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default='')
 
 DEBUG = os.environ.get("DEBUG", 'False').lower() in ['true', 'yes', '1']
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "ref.edumy.uz", ".ref.edumy.uz"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "ref.edumy.uz"]
 
 ENVIRONMENT = os.environ.get("DJANGO_ENVIRONMENT", default="local")
 
@@ -199,22 +199,9 @@ SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = "Lax"
 SESSION_COOKIE_AGE = 3600
-SECURE_SSL_REDIRECT = True  # HTTP so'rovlarni avtomatik HTTPS ga yo'naltirish
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # Reverse proxy orqali HTTPS sozlash
-SECURE_HSTS_SECONDS = 31536000  # HSTPSTS (HT Strict Transport Security) faollashtirish
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Subdomenlarga ham HSTS
-SECURE_HSTS_PRELOAD = True  # HSTS preload ro'yxatiga qo'shish
-SECURE_CONTENT_TYPE_NOSNIFF = True  # Xavfsizlik uchun MIME turi tekshiruvi
-SECURE_BROWSER_XSS_FILTER = True  # XSS hujumlariga qarshi himoya
-CSRF_COOKIE_SECURE = True  # CSRF cookie faqat HTTPS orqali ishlaydi
+SECURE_SSL_REDIRECT = not DEBUG  # HTTPS ga yo'naltirish (faqat prodaksiyada)
 
-
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "https://ref.edumy.uz",
-    "https://www.ref.edumy.uz",
-]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000", "https://ref.edumy.uz"]
 
 
 
